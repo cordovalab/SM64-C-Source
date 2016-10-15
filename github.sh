53e8f17ed957871ae498d942db58e52e26e0ff8a
Yoshimaster96 <yoshimaster96smwc@gmail.com>
2016-10-15 15:13:56 -0500

    Fixed a little itty bit of code

---
+++ github.sh
@@ -0,0 +1,7 @@
+git init
+git add -A
+git commit -m "Fixed a little itty bit of code"
+git remote add origin https://github.com/Yoshimaster96/SM64-C-Source.git
+git remote -v
+git pull https://github.com/Yoshimaster96/SM64-C-Source.git master
+git push
--- main.c
+++ main.c
@@ -1,36 +1,105 @@
 #include <stdint.h>
 #include "includes.h"
 
-uint32_t bss0[0x16770] = {0}; //0x8033A580
-uint32_t spIdle = 0x80200600, spMain = 0x80200E00, sp4 = 0x80202E00, sp5 = 0x80204E00;
-osThread * idleThread, * mainThread, * Thread4, * Thread5; //0x80246CF0, 0x802469B8, 0x8033AC40, 0x8033AA90
+uint32_t bss0[0x16770] = {0};
+uint8_t spIdle[0x800], spMain[0x2000], sp4[0x2000], sp5[0x2000];
+osThread * idleThread, * mainThread, * Thread4, * Thread5;
 uint32_t idleThread_cond0 = 0;
-uint8_t hasMainThreadBeenStarted = 0; //0x8032D590
+uint8_t hasMainThreadBeenStarted = 0;
+osMesgQueue msgQueue1, mQS1, mQS2, mQS3;
+OsMesg msg1, mS1, mS2, mS3;
 
-void Thread3_Main()
+void CreateThread(OSThread *t, OSId id, void (*entry)(void *), void *arg, void *sp, OSPri pri)
 {
+	t->next = NULL;
+	t->queue = NULL;
+	osCreateThread(t, id, entry, arg, sp, pri);
+}
+
+void SetupMessageQueues()
+{
+	osCreateMesgQueue(&mQS1,mS1,1);
+	osCreateMesgQueue(&mQS2,mS2,1);
+	osSetEventMesg(OS_EVENT_SI,mS2,NULL);
+	osCreateMesgQueue(&mQS3,mS3,0x10);
+	osCreateMesgQueue(&msgQueue1,msg1,0x10);
+	osViSetEventMsg(&msgQueue1,102,1);
+	osSetEventMesg(OS_EVENT_SP,msg1,100);
+	osSetEventMesg(OS_EVENT_DP,msg1,101);
+	osSetEventMesg(OS_EVENT_PRENMI,msg1,104);
+}
+
+uint32_t AllocPool()
+{
+	//TODO
+	InitMemPool(0x8005C000,0x801C1000);
+	proc_80278B98(0x4000,0);
+	return (uint32_t*)0x8033B494;
+}
+
+void Thread4()
+{
+	InitAudioSystem();
+}
+
+void Thread5_Debug()
+{
+	
+}
+
+void Thread3_Main(void * arg)
+{
+	int msg;
 	SetupMessageQueues();
 	AllocPool();
 	CopyScriptInterpreter();
-	osCreateThread(Thread4,4,Thread4(),NULL,sp4+0x2000,20);
+	CreateThread(Thread4,4,Thread4(),NULL,sp4+0x2000,20);
 	osStartThread(Thread4);
-	osCreateThread(Thread5,5,Thread5(),NULL,sp5+0x2000,20);
+	CreateThread(Thread5,5,Thread5_Debug(),NULL,sp5+0x2000,20);
 	osStartThread(Thread5);
-	osRecvMesg(Thread5,36,1);
-	//left off here
+	while(1)
+	{
+		uint32_t check = osRecvMesg(&msgQueue,&msg,OS_MESG_BLOCK);
+		switch msg
+		{
+			case 100:
+			{
+				proc_802467FC();
+				break;
+			}
+			case 101:
+			{
+				proc_8024694C();
+				break;
+			}
+			case 102:
+			{
+				proc_8024669C();
+				break;
+			}
+			case 103:
+			{
+				KickTask();
+				break;
+			}
+			case 104:
+			{
+				proc_8024639C();
+				break;
+			}
+		}
+	}
 }
 
-void Thread1_Idle()
+void Thread1_Idle(void * arg)
 {
 	osCreateViManager(OS_PRIORITY_VIMGR);
-	//TODO Find out what these modes are (look at RAM perhaps)
-	if(idleThread_cond0==1) osViSetMode((*(uint32_t*)(0x80335010))+0xA0);
-	else osViSetMode((*(uint32_t*)(0x80335010))+0x500);
-	//end TODO block
+	if(idleThread_cond0==1) osViSetMode(OS_VI_NTSC_LAN1);
+	else osViSetMode(OS_VI_MPAL_LAN1);
 	osViBlack(1);
 	osViSetSpecialFeatures(OS_VI_DITHER_FILTER_ON | OS_VI_GAMMA_OFF);
 	osCreatePiManager(150,0x8033ADF0,0x8033AE40,32);
-	osCreateThread(mainThread,3,Thread3_Main(),NULL,spMain+0x2000,100);
+	CreateThread(mainThread,3,Thread3_Main(),NULL,spMain+0x2000,100);
 	if(hasMainThreadBeenStarted==0) osStartThread(mainThread);
 	osSetThreadPri(0,0);
 	while(1);
@@ -39,14 +108,11 @@ void Thread1_Idle()
 void MainFunc()
 {
 	osInitialize();
-	// Dummy routine was here
-	osCreateThread(idleThread,1,Thread1_Idle(),NULL,spIdle+0x800,100);
+	CreateThread(idleThread,1,Thread1_Idle(),NULL,spIdle+0x800,100);
 	osStartThread(idleThread);
-	if(
 }
 
-void main() // EntryPoint
+void boot()
 {
-	// Jump to our actual "Main" function
 	MainFunc();
 }
